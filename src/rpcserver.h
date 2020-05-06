61691fd0602b22d7353312b9c85cec27c0f88ef9
Graham Higgins <gjh@bel-epa.com>
2020-05-06 12:33:34 +0100

    add dumpbootstrap RPC command

--- src/rpcserver.h
+++ src/rpcserver.h
@@ -141,7 +141,7 @@ extern json_spirit::Value sendtoaddress(const json_spirit::Array& params, bool f
 extern json_spirit::Value signmessage(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value verifymessage(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value makekeypair(const json_spirit::Array& params, bool fHelp);
-extern json_spirit::Value linearizehashes(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value dumpbootstrap(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value getreceivedbyaddress(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value getreceivedbyaccount(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value getbalance(const json_spirit::Array& params, bool fHelp);
